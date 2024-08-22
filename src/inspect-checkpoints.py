8a622535c094d02e0d7dd198f8586ea4371f619a
frillecode <frihae25@gmail.com>
2024-08-22 09:13:01 +0200

    inspecting checkpoints

--- src/inspect-checkpoints.py
+++ src/inspect-checkpoints.py
@@ -2,31 +2,24 @@ import os
 import yaml
 from utils.experiments import Experiment
 import matplotlib.pyplot as plt
-from transformers import (
-    AutoTokenizer,
-    AutoModelForSequenceClassification,
-    Trainer,
-    TrainerCallback,
-    TrainingArguments,
-)
 import json
 
 # Load specific run of the experiment
 with open(
     os.path.join(
         "configs",
-        "finetune_configs",  # Important that this is infodynamics_configs
-        "distilbert-base-uncased-MNLI_finetune_config.yaml",  # Has to contain a timestamp!
+        "infodynamics_configs",  # Important that this is infodynamics_configs
+        "distilbert-base-uncased-MNLI_infodynamics_config.yaml",  # Has to contain a timestamp!
     ),
     "r",
 ) as file:
     experiment_config = yaml.safe_load(file)
 
-experiment = Experiment(**experiment_config, timestamp="2024-07-31_12-23-26")
+experiment = Experiment(**experiment_config)
 
 # Load model checkpoint 
 # load checkpoint
-model_checkpoint = os.path.join(experiment.current_run_dir, "checkpoints", "checkpoint-33135", "trainer_state.json")
+model_checkpoint = os.path.join(experiment.current_run_dir, "checkpoints", "checkpoint-5000", "trainer_state.json")
 
 # Load trainer_state file
 with open(model_checkpoint, "r") as file:
@@ -50,12 +43,12 @@ plt.plot([x["step"] for x in merged_log_history], [x["loss"] for x in merged_log
 plt.plot([x["step"] for x in merged_log_history], [x["eval_accuracy"] for x in merged_log_history], label="eval_accuracy")
 plt.legend()
 plt.xlabel("Step")
-#plt.savefig(os.path.join(experiment.current_run_dir, "loss_plot.png"))
+plt.savefig(os.path.join(experiment.current_run_dir, "loss_plot.png"))
 plt.show()
 
 
 # Only plot first 5000 steps
-step_cutoff = 5000
+step_cutoff = 2000
 plt.plot([x["step"] for x in merged_log_history if x["step"] <= step_cutoff], [x["eval_loss"] for x in merged_log_history if x["step"] <= step_cutoff], label="eval_loss")
 plt.plot([x["step"] for x in merged_log_history if x["step"] <= step_cutoff], [x["loss"] for x in merged_log_history if x["step"] <= step_cutoff], label="loss")
 plt.plot([x["step"] for x in merged_log_history if x["step"] <= step_cutoff], [x["eval_accuracy"] for x in merged_log_history if x["step"] <= step_cutoff], label="eval_accuracy")
